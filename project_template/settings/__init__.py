0e7ad705bcd614080876db844d349472858bbf6e
Krzysztof Madejski <krzysztof@madejscy.pl>
2017-12-15 17:56:59 +0100

    Production & development: Separated settings and requirements

---
+++ project_template/settings/__init__.py
@@ -0,0 +1,2 @@
+# Let's default to developers setup
+from .dev import *
--- project_template/settings/base.py
+++ project_template/settings/base.py
@@ -13,17 +13,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'TODO_GENERATE_RANDOM_HASH_HERE'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 ALLOWED_HOSTS = []
 
@@ -70,17 +60,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'project_template.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
