05d93376c40c82bd82c3ee91f2212dde86f67c61
Krzysztof Madejski <krzysztof@madejscy.pl>
2018-01-05 15:49:53 +0100

    Logging + proper static files links

--- project_template/settings/heroku.py
+++ project_template/settings/heroku.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 from .stage import *
+import sys
 
 MOONSHEEP_DEVELOPMENT_MODE = True
 
@@ -18,28 +19,26 @@ MOONSHEEP_DEVELOPMENT_MODE = True
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': True,
-#     'formatters': {
-#         'verbose': {
-#             'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
-#         }
-#     },
-#     'handlers': {
-#         'gunicorn': {
-#             'level': 'DEBUG',
-#             'class': 'logging.handlers.RotatingFileHandler',
-#             'formatter': 'verbose',
-#             'filename': '/opt/djangoprojects/reports/bin/gunicorn.errors',
-#             'maxBytes': 1024 * 1024 * 100,  # 100 mb
-#         }
-#     },
-#     'loggers': {
-#         'gunicorn.errors': {
-#             'level': 'DEBUG',
-#             'handlers': ['gunicorn'],
-#             'propagate': True,
-#         },
-#     }
-# }
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'stream': sys.stderr,
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django': {
+            'level': 'INFO',
+            'handlers': ['console'],
+            'propagate': True,
+        },
+    }
+}
